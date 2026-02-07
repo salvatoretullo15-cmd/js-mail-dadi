944bb3910704a71804a66b85a9c9a993c388b341
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-07 16:18:28 +0100

    commit iniziale e impostazione delle email

---
+++ main.js
@@ -0,0 +1,32 @@
+console.log("it works");
+//Gioco dei dadi
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//numeri casuali per il player
+//numeri casuali per il computer
+//Stabilire il vincitore, in base a chi fa il punteggio più alto
+
+/*
+//Mail
+//Crea una lista di email di invitati ad una festa
+//Chiedi all’utente la sua email
+//controlla che sia nella lista di chi può accedere
+//stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for
+//Non è consentito usare nessun metodo proprio degli array (come includes, per esempio)
+//Si può fare? Certo che si basta ragionare un po’
+//Nota: 
+//Non è necessario provvedere alla validazione delle email
+*/
+
+
+//Crea una lista di email di invitati ad una festa.
+const guests = [
+    "osvaldo@hotmail.com",
+    "biggiogero@libero.com",
+    "olindo@gmail.com",
+    "damerjeffrey@hotmail.com",
+    "totorina@libero.com",
+    "edgain@gmail.com",
+    "menedezbrother@libero.com",
+]
